51ad3863ff735c6aeee50731d42ddbd7e61b209b
pucumt <weple@qq.com>
2018-12-11 15:50:27 +0800

    init

---
+++ GenerateEnrollExcel/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace GenerateScore
+{
+    partial class Frm_Generator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_load = new System.Windows.Forms.Button();
+            this.ofD_file = new System.Windows.Forms.OpenFileDialog();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.cmbGrade = new System.Windows.Forms.ComboBox();
+            this.cmbSheet = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbScore = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbTest = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(66, 134);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 30);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "加 载";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // ofD_file
+            // 
+            this.ofD_file.FileName = "报名情况5.xlsx";
+            this.ofD_file.Filter = "*.xls|*.xlsx";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Font = new System.Drawing.Font("SimSun", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtFile.Location = new System.Drawing.Point(66, 82);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(312, 27);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(384, 81);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 30);
+            this.btn_select.TabIndex = 2;
+            this.btn_select.Text = "选择...";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(535, 318);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtFile);
+            this.tabPage1.Controls.Add(this.btn_select);
+            this.tabPage1.Controls.Add(this.btn_load);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(527, 289);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "选择Excel";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.cmbTest);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.cmbScore);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.lblScore);
+            this.tabPage2.Controls.Add(this.cmbGrade);
+            this.tabPage2.Controls.Add(this.cmbSheet);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(527, 289);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "生成成绩";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(209, 190);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(0, 15);
+            this.lblScore.TabIndex = 5;
+            // 
+            // cmbGrade
+            // 
+            this.cmbGrade.FormattingEnabled = true;
+            this.cmbGrade.Location = new System.Drawing.Point(203, 149);
+            this.cmbGrade.Name = "cmbGrade";
+            this.cmbGrade.Size = new System.Drawing.Size(195, 23);
+            this.cmbGrade.TabIndex = 3;
+            this.cmbGrade.SelectedIndexChanged += new System.EventHandler(this.cmbGrade_SelectedIndexChanged);
+            // 
+            // cmbSheet
+            // 
+            this.cmbSheet.FormattingEnabled = true;
+            this.cmbSheet.Location = new System.Drawing.Point(203, 52);
+            this.cmbSheet.Name = "cmbSheet";
+            this.cmbSheet.Size = new System.Drawing.Size(281, 23);
+            this.cmbSheet.TabIndex = 2;
+            this.cmbSheet.SelectedIndexChanged += new System.EventHandler(this.cmbSheet_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "选择年级：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择sheet：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "忽略成绩：";
+            // 
+            // cmbScore
+            // 
+            this.cmbScore.FormattingEnabled = true;
+            this.cmbScore.Items.AddRange(new object[] {
+            "40",
+            "35"});
+            this.cmbScore.Location = new System.Drawing.Point(203, 6);
+            this.cmbScore.Name = "cmbScore";
+            this.cmbScore.Size = new System.Drawing.Size(121, 23);
+            this.cmbScore.TabIndex = 7;
+            this.cmbScore.SelectedIndexChanged += new System.EventHandler(this.cmbScore_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "结果：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "选择测试：";
+            // 
+            // cmbTest
+            // 
+            this.cmbTest.FormattingEnabled = true;
+            this.cmbTest.Location = new System.Drawing.Point(203, 99);
+            this.cmbTest.Name = "cmbTest";
+            this.cmbTest.Size = new System.Drawing.Size(195, 23);
+            this.cmbTest.TabIndex = 10;
+            this.cmbTest.SelectedIndexChanged += new System.EventHandler(this.cmbTest_SelectedIndexChanged);
+            // 
+            // Frm_Generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 318);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "Frm_Generator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "生成成绩";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.OpenFileDialog ofD_file;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.ComboBox cmbGrade;
+        private System.Windows.Forms.ComboBox cmbSheet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbTest;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GenerateEnrollExcel/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using System.Data.OleDb;
+using System.Threading;
+using System.Data.SqlClient;
+
+namespace GenerateScore
+{
+    public partial class Frm_Generator : Form
+    {
+        private XSSFWorkbook hssfworkbook = null;
+        private DataTable dt;
+        private XSSFSheet sheet1;
+        public Frm_Generator()
+        {
+            InitializeComponent();
+            cmbScore.SelectedIndex = 0;
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            var result = ofD_file.ShowDialog();
+            txtFile.Text = ofD_file.FileName;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtFile.Text.Trim()))
+            {
+                return;
+            }
+
+            try
+            {
+                using (FileStream fs = File.Open(@txtFile.Text, FileMode.Open,
+            FileAccess.Read, FileShare.ReadWrite))
+                {
+                    //把xls文件读入workbook变量里，之后就可以关闭了  
+                    hssfworkbook = new XSSFWorkbook(fs);
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error:"+ex.Message);
+            }
+
+            // 获取sheetName
+            cmbSheet.Items.Clear();
+            cmbSheet.SelectedIndex = -1;
+            cmbSheet.Text = "";
+            for (var i=0;i< hssfworkbook.NumberOfSheets;i++)
+            {
+                // calculate average scores of each sheet
+                // cmbSheet.Items.Add(hssfworkbook.GetSheetName(i));
+                calculateSheet(i);
+            }
+
+            // 跳转tab
+            tabControl1.SelectTab(1);
+            tabPage2.Show();
+        }
+
+        private void calculateSheet(int i)
+        {
+            if (hssfworkbook == null)
+            {
+                return;
+            }
+
+            sheet1 = hssfworkbook.GetSheetAt(i) as XSSFSheet;
+            if (sheet1 == null)
+            {
+                return;
+            }
+
+            setDataTable(1);
+
+            DataView dataView = dt.DefaultView;
+            DataTable dataTableDistinct = dataView.ToTable(true, "grade");
+
+            for (var j = 0; j < dataTableDistinct.Rows.Count; j++)
+            {
+               // 按照年级排名
+
+            }
+        }
+
+        private void calculateGrade()
+        {
+
+        }
+
+        private void cmbSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblScore.Text = "";
+            if (hssfworkbook==null)
+            {
+                return;
+            }
+
+            sheet1 = hssfworkbook.GetSheetAt(0) as XSSFSheet;
+
+            if(sheet1 == null)
+            {
+                return;
+            }
+
+            // grade
+            cmbGrade.Items.Clear();
+            cmbGrade.Text = "";
+            cmbGrade.SelectedIndex = -1;
+
+            setDataTable();
+
+            DataView dataView = dt.DefaultView;
+            DataTable dataTableDistinct = dataView.ToTable(true, "grade");
+
+            for(var j=0;j< dataTableDistinct.Rows.Count;j++)
+            {
+                cmbGrade.Items.Add(dataTableDistinct.Rows[j]["grade"]);
+            }
+
+            // test name
+            cmbTest.Items.Clear();
+            cmbTest.Text = "";
+            cmbTest.SelectedIndex = -1;
+
+            var m = 4;
+            var testCell = sheet1.GetRow(0).GetCell(m);
+            while(testCell != null)
+            {
+                cmbTest.Items.Add(testCell.StringCellValue);
+                m++;
+                testCell = sheet1.GetRow(0).GetCell(m);
+            }
+            cmbTest.SelectedIndex = 0;
+        }
+
+        private void cmbGrade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbGrade.SelectedIndex == -1 || cmbTest.SelectedIndex == -1 || cmbSheet.SelectedIndex == -1)
+            {
+                return;
+            }
+            lblScore.Text = "";
+            DataView dv = dt.DefaultView;
+            dv.RowFilter = "grade = '" + cmbGrade.Text + "' and score> "+cmbScore.Text;
+            dv.Sort = "score desc";
+            DataTable dtLast = dv.ToTable(false, "score");
+            var total = dtLast.Rows.Count;
+            int p30 = (int)Math.Ceiling(total * 0.3);
+            int p60 = (int)Math.Ceiling(total * 0.6);
+
+            lblScore.Text = "30%: "+ dtLast.Rows[p30]["score"] +"\r\n60%: " + dtLast.Rows[p60]["score"];
+        }
+
+        private void cmbScore_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbTest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void setDataTable(int j)
+        {
+            // 输入成绩到 table
+            dt = new DataTable();
+            dt.Columns.Add(new DataColumn("grade"));
+            dt.Columns.Add(new DataColumn("score", typeof(float)));
+            dt.Columns.Add(new DataColumn("className"));
+            var i = 1;
+            var s = 6 + j;// score column index
+            while (sheet1.GetRow(i) != null)
+            {
+                DataRow row = dt.NewRow();
+                row["grade"] = sheet1.GetRow(i).GetCell(3).StringCellValue;
+                row["className"] = sheet1.GetRow(i).GetCell(0).StringCellValue;
+                var cell = sheet1.GetRow(i).GetCell(s);
+                if (cell.CellType == NPOI.SS.UserModel.CellType.Numeric)
+                {
+                    row["score"] = sheet1.GetRow(i).GetCell(s).NumericCellValue;
+                }
+                else
+                {
+                    row["score"] = float.Parse(sheet1.GetRow(i).GetCell(s).StringCellValue);
+                }
+                
+                dt.Rows.Add(row);
+                i++;
+            }
+        }
+    }
+}
