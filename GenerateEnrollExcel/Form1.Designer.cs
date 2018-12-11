5f1037dd376ad6ad9d5522b65b513823aac476c1
pucumt <weple@qq.com>
2018-12-11 17:15:20 +0800

    finished he basic function of 30% score

--- GenerateEnrollExcel/Form1.Designer.cs
+++ GenerateEnrollExcel/Form1.Designer.cs
@@ -36,15 +36,8 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.lblScore = new System.Windows.Forms.Label();
-            this.cmbGrade = new System.Windows.Forms.ComboBox();
-            this.cmbSheet = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.cmbScore = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.cmbTest = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -97,6 +90,8 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.cmbScore);
+            this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.txtFile);
             this.tabPage1.Controls.Add(this.btn_select);
             this.tabPage1.Controls.Add(this.btn_load);
@@ -110,16 +105,7 @@
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.cmbTest);
-            this.tabPage2.Controls.Add(this.label5);
-            this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Controls.Add(this.cmbScore);
-            this.tabPage2.Controls.Add(this.label3);
             this.tabPage2.Controls.Add(this.lblScore);
-            this.tabPage2.Controls.Add(this.cmbGrade);
-            this.tabPage2.Controls.Add(this.cmbSheet);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -136,89 +122,25 @@
             this.lblScore.Size = new System.Drawing.Size(0, 15);
             this.lblScore.TabIndex = 5;
             // 
-            // cmbGrade
-            // 
-            this.cmbGrade.FormattingEnabled = true;
-            this.cmbGrade.Location = new System.Drawing.Point(203, 149);
-            this.cmbGrade.Name = "cmbGrade";
-            this.cmbGrade.Size = new System.Drawing.Size(195, 23);
-            this.cmbGrade.TabIndex = 3;
-            this.cmbGrade.SelectedIndexChanged += new System.EventHandler(this.cmbGrade_SelectedIndexChanged);
-            // 
-            // cmbSheet
-            // 
-            this.cmbSheet.FormattingEnabled = true;
-            this.cmbSheet.Location = new System.Drawing.Point(203, 52);
-            this.cmbSheet.Name = "cmbSheet";
-            this.cmbSheet.Size = new System.Drawing.Size(281, 23);
-            this.cmbSheet.TabIndex = 2;
-            this.cmbSheet.SelectedIndexChanged += new System.EventHandler(this.cmbSheet_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(66, 152);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "选择年级：";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 55);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "选择sheet：";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(66, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 15);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "忽略成绩：";
-            // 
             // cmbScore
             // 
             this.cmbScore.FormattingEnabled = true;
             this.cmbScore.Items.AddRange(new object[] {
             "40",
             "35"});
-            this.cmbScore.Location = new System.Drawing.Point(203, 6);
+            this.cmbScore.Location = new System.Drawing.Point(203, 30);
             this.cmbScore.Name = "cmbScore";
             this.cmbScore.Size = new System.Drawing.Size(121, 23);
-            this.cmbScore.TabIndex = 7;
-            this.cmbScore.SelectedIndexChanged += new System.EventHandler(this.cmbScore_SelectedIndexChanged);
+            this.cmbScore.TabIndex = 9;
             // 
-            // label4
+            // label3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(66, 190);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 15);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "结果：";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(66, 102);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 15);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "选择测试：";
-            // 
-            // cmbTest
-            // 
-            this.cmbTest.FormattingEnabled = true;
-            this.cmbTest.Location = new System.Drawing.Point(203, 99);
-            this.cmbTest.Name = "cmbTest";
-            this.cmbTest.Size = new System.Drawing.Size(195, 23);
-            this.cmbTest.TabIndex = 10;
-            this.cmbTest.SelectedIndexChanged += new System.EventHandler(this.cmbTest_SelectedIndexChanged);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "忽略成绩：";
             // 
             // Frm_Generator
             // 
@@ -249,15 +171,8 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label lblScore;
-        private System.Windows.Forms.ComboBox cmbGrade;
-        private System.Windows.Forms.ComboBox cmbSheet;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cmbScore;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox cmbTest;
-        private System.Windows.Forms.Label label5;
     }
 }
 
--- GenerateEnrollExcel/Form1.cs
+++ GenerateEnrollExcel/Form1.cs
@@ -18,8 +18,11 @@ namespace GenerateScore
     public partial class Frm_Generator : Form
     {
         private XSSFWorkbook hssfworkbook = null;
+        private XSSFWorkbook newwb = null;
         private DataTable dt;
         private XSSFSheet sheet1;
+        private XSSFSheet newst;
+        private int insertIndex = 1;
         public Frm_Generator()
         {
             InitializeComponent();
@@ -60,15 +63,15 @@ namespace GenerateScore
             }
 
             // 获取sheetName
-            cmbSheet.Items.Clear();
-            cmbSheet.SelectedIndex = -1;
-            cmbSheet.Text = "";
+            newwb = new XSSFWorkbook();
             for (var i=0;i< hssfworkbook.NumberOfSheets;i++)
             {
                 // calculate average scores of each sheet
-                // cmbSheet.Items.Add(hssfworkbook.GetSheetName(i));
                 calculateSheet(i);
             }
+            FileStream file = new FileStream(@"test.xlsx", FileMode.Create);
+            newwb.Write(file);
+            file.Close();
 
             // 跳转tab
             tabControl1.SelectTab(1);
@@ -87,94 +90,79 @@ namespace GenerateScore
             {
                 return;
             }
+            // 新的sheet
+            newst = newwb.CreateSheet(sheet1.SheetName) as XSSFSheet;
+            // 课程名  年级  老师  手机号 考试名称 优等人数 良等人数 有效人数
+            newst.GetRow(0).GetCell(0).SetCellValue("课程名");
+            newst.GetRow(0).GetCell(1).SetCellValue("年级");
+            newst.GetRow(0).GetCell(2).SetCellValue("老师");
+            newst.GetRow(0).GetCell(3).SetCellValue("手机号");
+            newst.GetRow(0).GetCell(4).SetCellValue("考试名称");
+            newst.GetRow(0).GetCell(5).SetCellValue("优等人数");
+            newst.GetRow(0).GetCell(6).SetCellValue("良等人数");
+            newst.GetRow(0).GetCell(7).SetCellValue("有效人数");
+            insertIndex = 1;
+            int m = 7;
+            var testCell = sheet1.GetRow(0).GetCell(m);
+            while (testCell != null)
+            {   // 测试分别运算出结果
+                calculateTest(m, testCell.StringCellValue);
+                m++;
+                testCell = sheet1.GetRow(0).GetCell(m);
+            }
+        }
 
-            setDataTable(1);
+        private void calculateTest(int m, string testName)
+        {
+            setDataTable(m);
 
             DataView dataView = dt.DefaultView;
             DataTable dataTableDistinct = dataView.ToTable(true, "grade");
 
             for (var j = 0; j < dataTableDistinct.Rows.Count; j++)
             {
-               // 按照年级排名
-
+                // 按照年级排名
+                calculateGrade(testName, dataTableDistinct.Rows[j]["grade"].ToString());
             }
         }
 
-        private void calculateGrade()
+        private void calculateGrade(string testName, string gradeTxt)
         {
-
-        }
-
-        private void cmbSheet_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            lblScore.Text = "";
-            if (hssfworkbook==null)
-            {
-                return;
-            }
-
-            sheet1 = hssfworkbook.GetSheetAt(0) as XSSFSheet;
-
-            if(sheet1 == null)
-            {
-                return;
-            }
-
-            // grade
-            cmbGrade.Items.Clear();
-            cmbGrade.Text = "";
-            cmbGrade.SelectedIndex = -1;
-
-            setDataTable();
-
-            DataView dataView = dt.DefaultView;
-            DataTable dataTableDistinct = dataView.ToTable(true, "grade");
-
-            for(var j=0;j< dataTableDistinct.Rows.Count;j++)
-            {
-                cmbGrade.Items.Add(dataTableDistinct.Rows[j]["grade"]);
-            }
-
-            // test name
-            cmbTest.Items.Clear();
-            cmbTest.Text = "";
-            cmbTest.SelectedIndex = -1;
-
-            var m = 4;
-            var testCell = sheet1.GetRow(0).GetCell(m);
-            while(testCell != null)
-            {
-                cmbTest.Items.Add(testCell.StringCellValue);
-                m++;
-                testCell = sheet1.GetRow(0).GetCell(m);
-            }
-            cmbTest.SelectedIndex = 0;
-        }
-
-        private void cmbGrade_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if(cmbGrade.SelectedIndex == -1 || cmbTest.SelectedIndex == -1 || cmbSheet.SelectedIndex == -1)
-            {
-                return;
-            }
-            lblScore.Text = "";
             DataView dv = dt.DefaultView;
-            dv.RowFilter = "grade = '" + cmbGrade.Text + "' and score> "+cmbScore.Text;
+            dv.RowFilter = "grade = '" + gradeTxt + "' and score> " + cmbScore.Text;
             dv.Sort = "score desc";
             DataTable dtLast = dv.ToTable(false, "score");
             var total = dtLast.Rows.Count;
             int p30 = (int)Math.Ceiling(total * 0.3);
             int p60 = (int)Math.Ceiling(total * 0.6);
 
-            lblScore.Text = "30%: "+ dtLast.Rows[p30]["score"] +"\r\n60%: " + dtLast.Rows[p60]["score"];
-        }
+            int p30Score = (int)dtLast.Rows[p30]["score"]; // 优的分数
+            int p60Score = (int)dtLast.Rows[p60]["score"]; // 良的分数
 
-        private void cmbScore_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
+            // 拉出所有班级信息
+            DataView dvClass = dt.DefaultView;
+            DataTable dataTableClasses = dvClass.ToTable(true, "className");
 
-        private void cmbTest_SelectedIndexChanged(object sender, EventArgs e)
-        {
+            for (var j = 0; j < dataTableClasses.Rows.Count; j++)
+            {
+                string className = dataTableClasses.Rows[j]["className"].ToString();
+                string teacher = dataTableClasses.Rows[j]["teacher"].ToString();
+                string mobile = dataTableClasses.Rows[j]["mobile"].ToString();
+                newst.GetRow(insertIndex).GetCell(0).SetCellValue(className);
+                newst.GetRow(insertIndex).GetCell(1).SetCellValue(gradeTxt);
+                newst.GetRow(insertIndex).GetCell(2).SetCellValue(teacher);
+                newst.GetRow(insertIndex).GetCell(3).SetCellValue(mobile);
+                newst.GetRow(insertIndex).GetCell(4).SetCellValue(testName);
+                // 算出优率和良率，然后输出
+                DataView dvScore = dt.DefaultView;
+                dvScore.RowFilter = "grade = '" + gradeTxt + "' and className='" + className + "' and score>= " + p30Score;
+                newst.GetRow(insertIndex).GetCell(5).SetCellValue(dvScore.Count);
+                dvScore.RowFilter = "grade = '" + gradeTxt + "' and className='" + className + "' and score<"+ p30Score + " and score>= " + p60Score;
+                newst.GetRow(insertIndex).GetCell(6).SetCellValue(dvScore.Count);
+                dvScore.RowFilter = "grade = '" + gradeTxt + "' and className='" + className + "' and score>= " + cmbScore.Text;
+                newst.GetRow(insertIndex).GetCell(7).SetCellValue(dvScore.Count);
+                insertIndex++;
+            }
         }
 
         private void setDataTable(int j)
@@ -184,13 +172,18 @@ namespace GenerateScore
             dt.Columns.Add(new DataColumn("grade"));
             dt.Columns.Add(new DataColumn("score", typeof(float)));
             dt.Columns.Add(new DataColumn("className"));
+            dt.Columns.Add(new DataColumn("teacher"));
+            dt.Columns.Add(new DataColumn("mobile"));
             var i = 1;
-            var s = 6 + j;// score column index
+            var s = j;// score column index
             while (sheet1.GetRow(i) != null)
             {
                 DataRow row = dt.NewRow();
                 row["grade"] = sheet1.GetRow(i).GetCell(3).StringCellValue;
                 row["className"] = sheet1.GetRow(i).GetCell(0).StringCellValue;
+                row["teacher"] = sheet1.GetRow(i).GetCell(5).StringCellValue;
+                row["mobile"] = sheet1.GetRow(i).GetCell(6).StringCellValue;
+
                 var cell = sheet1.GetRow(i).GetCell(s);
                 if (cell.CellType == NPOI.SS.UserModel.CellType.Numeric)
                 {
